a0f65a635c9cf21f429836ad08a39010b19fe4de
alexxpereira15-svg <alexxpereira15@gmail.com>
2026-08-28 16:59:14 -0600

    Update budget.ts

--- app/actions/budget.ts
+++ app/actions/budget.ts
@@ -1,6 +1,6 @@
 'use server'
 
-import { prisma } from '@/lib/prisma'
+import { prisma } from '../../lib/prisma'
 import { CategoryType } from '@prisma/client'
 
 // Obtiene o crea un presupuesto mensual para el usuario
